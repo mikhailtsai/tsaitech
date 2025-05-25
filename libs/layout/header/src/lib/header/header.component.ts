01ab283421b6129d2ce35994e85ef34983c9b5f1
mikhail.tsai <mikhail.tsai@gmail.com>
2025-05-24 22:10:15 -0300

    feat: hero section update

--- libs/layout/header/src/lib/header/header.component.ts
+++ libs/layout/header/src/lib/header/header.component.ts
@@ -1,25 +1,18 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { IconComponent } from '@tsaitech/common/ui-kit';
 
 import { LogoComponent } from '../logo';
-import { SearchComponent } from './components';
 import telegramSvg from './assets/telegram.svg';
 import linkedInSvg from './assets/linkedin.svg';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'tt-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    SearchComponent,
-    LogoComponent,
-    IconComponent,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  imports: [LogoComponent, IconComponent, RouterLink, RouterLinkActive],
 })
 export class HeaderComponent {
   readonly assets = {
